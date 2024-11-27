3a37da8457157d7d45ec6179536be74c560dcbea
Владислав Васин <VasinVladislav94@gmail.com>
2024-11-27 13:16:27 +0300

    Добавьте файлы проекта.

---
+++ HomeWorkCSharp-1.11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkCSharp_1._11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.11. Составить программу вывода на экран «столбиком» четырех любых чисел.
+            Random rnd = new Random();
+            for (int i = 0; i < 4; i++)
+            {
+                int a = rnd.Next();
+                Console.WriteLine($"{a}");
+            }
+        }
+    }
+}
---
+++ HomeWorkCSharp-1.11/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("HomeWorkCSharp-1.11")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HomeWorkCSharp-1.11")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("f943b792-9594-4a1f-aec8-f38b19e4ce43")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
